3d97c1eda2c832c0d59b346c881a542e2427e5f2
t4jgat <tmtmtmt1423@gmail.com>
2023-10-09 14:02:49 +0600

    some bug fixes

--- src/main/java/kz/t4jgat/Americano.java
+++ src/main/java/kz/t4jgat/Americano.java
@@ -2,7 +2,7 @@ package kz.t4jgat;
 
 // A class describing an americano object without wrappers
 public class Americano implements Coffee {
-    String description = "Americano\n";
+    String description = "Americano ($\"+ cost() +\")\n";
     @Override
     public String getDescription() {
         return description;
--- src/main/java/kz/t4jgat/Latte.java
+++ src/main/java/kz/t4jgat/Latte.java
@@ -2,7 +2,7 @@ package kz.t4jgat;
 
 // A class describing a latte object without wrappers
 public class Latte implements Coffee {
-    String description = "Latte\n";
+    String description = "Latte ($"+ cost() +")\n";
 
     public String getDescription() {
         return description;
